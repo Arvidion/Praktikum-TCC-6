14ea834397abbf37a3a8276a4dc0acf3a61ad7ae
ArvidionHavas <142262748+Arvidion@users.noreply.github.com>
2025-04-18 14:33:44 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('havas','root','',{
-    host: "104.197.117.136",
+const db = new Sequelize('RECOVER_YOUR_DATA','root','',{
+    host: "34.46.200.135",
     dialect: 'mysql'
 });
 
